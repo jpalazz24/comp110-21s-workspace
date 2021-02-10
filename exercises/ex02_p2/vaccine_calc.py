bb44ceb9942bdec3a1d2f8a18da4e9166493ecc3
Jake Palazzo <jpalazz2@live.unc.edu>
2021-02-10 11:22:53 -0500

    exercise 2 done

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -13,13 +13,16 @@ def main() -> None:
     target: int = int(input("Target percent vaccinated: "))
     days_remaining: int = int(days_to_target(population, doses, doses_per_day, target))
     target_date: str = str(future_date(days_remaining))
-    print("We will reach " + str(target) + "% vaccination in " + str(days_remaining) + " days, which falls on " + target_date + ".")
+    target_output: str = str("We will reach " + str(target) + "% vaccination in " + str(days_remaining))
+    date_output: str = str(" days, which falls on " + target_date + ".")
+    print(target_output + date_output)
 
 
 # TODO 1: Define days_to_target function
 def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
     """Calculation of days until target is reached."""
     return int(round((population * (target / 100) - (doses / 2)) / (doses_per_day / 2)))
+
 
 # TODO 3: Define future_date function
 def future_date(days_out: int) -> str:
@@ -28,6 +31,7 @@ def future_date(days_out: int) -> str:
     days_until: timedelta = timedelta(days_out)
     date: datetime = today + days_until
     return date.strftime("%B %d, %Y")
+
 
 if __name__ == "__main__":
     main()
